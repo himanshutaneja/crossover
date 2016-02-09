8a6b16830609c3fa95db2a9fac6973a6da03278d
himanshutaneja <himanshu.taneja1982@gmail.com>
2016-02-09 22:46:56 +0530

    basic setup

---
+++ src/main/java/com/dev/crossover/Application.java
@@ -0,0 +1,22 @@
+package com.dev.crossover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class Application {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+}
