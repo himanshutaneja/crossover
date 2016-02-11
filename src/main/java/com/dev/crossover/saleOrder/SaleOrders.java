2c8d5b3affee8b8cd83e8d8011815eef66a5adc4
Siddharth Malik <siddharthmlk@hotmail.com>
2016-02-11 20:16:52 +0530

    comitted

---
+++ src/main/java/com/dev/crossover/saleOrder/SaleOrders.java
@@ -0,0 +1,20 @@
+package com.dev.crossover.saleOrder;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "SALE_ORDERS")
+public class SaleOrders {
+
+	@Id
+	@Column(name = "LI_ORDER_NO")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int orderNo;
+	@Column(name = "CUST_CODE")
+	private int custCode;
+}
